1b546c49da2dc83d2a48ceef65e922a69a356285
Manoj Singh Negi <justanothermanoj@gmail.com>
2024-02-19 15:42:57 +0530

    Initial Commit

---
+++ butler/butlerApp.swift
@@ -0,0 +1,32 @@
+//
+//  butlerApp.swift
+//  butler
+//
+//  Created by Manoj on 19/02/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct butlerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
